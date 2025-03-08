1ab5c84c9e9de5c0428bd09b6e0e77a871896268
darinochk <your_email@example.com>
2025-03-08 17:14:19 +0300

    Add axios and zustand, set up auth and API context

---
+++ api/api.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+import {useUserStore} from "@/context/user-store";
+
+
+const BACK_URL = 'http://localhost:8080/';
+
+export interface Message {
+    message: string;
+    isError: boolean;
+}
+
+export const api = axios.create({
+    baseURL: BACK_URL
+})
+
+api.interceptors.request.use(function (config: any) {
+    if(useUserStore.getState().token) {
+        config.headers.Authorization = 'Bearer ' + useUserStore.getState().token
+    }
+    return config;
+}, function (error: any) {
+    return Promise.reject(error);
+});
+
+export interface Page<T> {
+    content: T[];
+    totalPages: number
+}
+
+export async function loadAllPages<T>(
+    fetchFunc: (page: number, arg: number) => Promise<Page<T[]>>,
+    arg: number
+): Promise<T[]> {
+    const getPage = async (page: number, content: T[]): Promise<T[]> => {
+        const data = await fetchFunc(page, arg);
+        // @ts-ignore
+        content.push(...data.content);
+        return page < data.totalPages ? getPage(page + 1, content) : content;
+    };
+    return await getPage(0, []);
+}
---
+++ context/user-store.ts
@@ -0,0 +1,56 @@
+import {create} from "zustand";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+export enum Role {
+	USER = "USER",
+	WAITER = "WAITER",
+	ADMIN = "ADMIN"
+}
+
+interface IntUser {
+	"id": number;
+	"firstName": string;
+	"lastName": string;
+	"email": string;
+	"role": Role;
+	"phone": string;
+	locked: boolean;
+}
+
+interface User {
+	inited: boolean;
+	user: IntUser | null;
+	token: string | null;
+	Login: (user: IntUser, token: string) => void;
+	Logout: () => void;
+	init: (router: AppRouterInstance, pathname: string) => void;
+}
+
+export const useUserStore = create<User>((set) => ({
+    inited: false,
+	user: null,
+	token: null,
+	Login: function (user, token,) {
+		localStorage.setItem("user", JSON.stringify(user));
+		localStorage.setItem("token", token);
+		set({user, token});
+	},
+	Logout: function () {
+		console.log('logout')
+		localStorage.removeItem("user");
+		localStorage.removeItem("token");
+		set({user: null, token: null});
+	},
+	init: function(router, pathname) {
+		try {
+			const user: IntUser = JSON.parse(localStorage.getItem("user") || "");
+			const token = localStorage.getItem("token");
+			set({token: token, inited: true, user});
+			if(!user && pathname !== "/login" && pathname !== "/register") router.push("/login");
+		} catch (e) {
+			console.warn(e);
+			set({inited: true});
+			if(pathname !== "/login" && pathname !== "/register") router.push("/login");
+		}
+	}
+}));
